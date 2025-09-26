811d6f7599bdd0d4ba7df42fe91951f67b7cb3a2
vansh1505 <vanshsharma.vs321@gmail.com>
2025-09-26 20:08:34 +0530

    motion bug fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react"
 import Link from "next/link"
 import Image from "next/image"
-import { motion } from "framer-motion"
+import { motion } from "motion/react"
 import {
   Check,
   ChevronRight,
